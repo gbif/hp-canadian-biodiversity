116a858f3e642e5aae2528d2bd821c2437ca2ab6
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2021-01-29 11:50:13 +0100

    Data scope initial attempt

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,19 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'taxonKey', value: 5 }
+  rootPredicate: {
+    "type": "and",
+    "predicates": [
+   		{
+        "type":"equals",
+        "key": "countryCode",
+        "value": "CA"
+      },
+      {
+        "type": "equals",
+        "key": "notIssues",
+        "value": "COUNTRY_COORDINATE_MISMATCH"
+      }
+    ]
+  }
 };
