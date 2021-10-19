8e7c971d5c9727b1d12ff9999a3296c6baa5d9ba
Belisle-Leclerc, Melanie <melanie.belisle-leclerc@canada.ca>
2021-10-19 13:17:59 -0400

    Revamp extract_checklist script

--- assets/py/extract_checklist_info.py
+++ assets/py/extract_checklist_info.py
@@ -1,11 +1,18 @@
 """
+Extracts data from dwca taxon.txt file of the checklists and writes it in markdown format.
+
+!Important! Requires modifications based on the columns of interest in the checklist.
+
 Usage example:
-python extract_checklist_info.py --input_file "~/Downloads/dwca-aafc-brassicaceae-checklist-v1.8/taxon.txt"
---dataset_key 94308742-058c-46d5-b763-06e9207a6b15 --output_file "_data/example.yml"
+python extract_checklist_info.py --input_file "~/Downloads/dwca-aafc-hymenoptera-canada-ak-gl-v2.0/taxon.txt" --dataset_key 94308742-058c-46d5-b763-06e9207a6b15 --output_file "_data/example.yml"
 """
 import argparse
+import numpy as np
 import pandas as pd
 import requests
+
+COLUMNS_OF_INTEREST = ['taxonID', 'scientificName', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus',
+                       'subgenus', 'taxonRank']
 
 
 def _parse_args():
@@ -19,7 +26,8 @@ def _parse_args():
     return parser.parse_args()
 
 
-def get_gbif_link_from_api_call_string(api_call_string):
+def get_gbif_link(taxon_id, dataset_key):
+    api_call_string = f"https://api.gbif.org/v1/species?datasetKey={dataset_key}&sourceId={taxon_id}"
     resp = requests.get(api_call_string)
     if resp.ok:
         results = resp.json()['results']
@@ -28,22 +36,24 @@ def get_gbif_link_from_api_call_string(api_call_string):
             return None
         k = results[0]['key']
         return f'https://www.gbif.org/species/{k}/verbatim'
-    return None
+    else:
+        print(f'Error getting GBIF link for taxonID {taxon_id}, dataset_key {dataset_key}.\n {resp.text}')
 
 
 def main():
     args = _parse_args()
     data = pd.read_csv(args.input_file, sep='\t')
-    df = data[['taxonID', 'scientificName', 'genus', 'taxonRank']]
-    df['base_api_string'] = f"https://api.gbif.org/v1/species?datasetKey={args.dataset_key}&sourceId="
-    df['taxonID'].map(str)
-    df.loc[:, 'search_string'] = df['base_api_string'] + df['taxonID'].astype(str)
-    df.loc[:, 'link'] = df.apply(lambda x: get_gbif_link_from_api_call_string(x['search_string']), axis=1)
-    df.loc[:, 'line'] = "| " + df['genus'] + " |[ " + df['scientificName'] + "](" + df['link'] + ") | " + df['taxonRank'] + " |"
-    df = df[['line']]
-    with open(args.output_file, 'a') as f:
-        dfAsString = df.to_string(header=False, index=False)
-        f.write(dfAsString)
+    df = data[COLUMNS_OF_INTEREST]
+    df['has_numeric_tID'] = np.where(df['taxonID'].str.isnumeric(), True, False)
+    df.loc[:, 'link'] = df.apply(lambda x: get_gbif_link(x['taxonID'], args.dataset_key)
+                        if x['has_numeric_tID'] else x['taxonID'], axis=1)
+    df.loc[df.taxonRank == 'family', 'Scientific Name'] = "[ "
+    df.loc[df.taxonRank == 'genus', 'Scientific Name'] = "&nbsp;[ "
+    df.loc[df.taxonRank == 'subgenus', 'Scientific Name'] = "&nbsp;&nbsp;[ "
+    df.loc[df.taxonRank == 'species', 'Scientific Name'] = "&nbsp;&nbsp;&nbsp;[ "
+
+    df['Scientific Name'] = df['Scientific Name'] + df['scientificName'] + "](" + df['link'] + ")"
+    df[['Scientific Name', 'taxonRank']].to_markdown(buf=args.output_file, index=False)
 
 
 if __name__ == '__main__':
