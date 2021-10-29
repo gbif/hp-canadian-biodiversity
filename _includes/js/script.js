bc1f2037b9114c068aa7ad2843801781e3a3caf7
Belisle-Leclerc, Melanie <melanie.belisle-leclerc@canada.ca>
2021-10-29 14:41:42 -0400

    Extract ids from data page in progress

---
+++ _includes/js/script.js
@@ -0,0 +1,54 @@
+
+ function getFilter() {
+    let searchParams = new URLSearchParams(window.location.search);
+    console.log(searchParams);
+    let encodedFilter = searchParams.get("filter");
+    if (!encodedFilter) return null;
+    return JSON.parse(atob(encodedFilter)).must;
+ }
+
+ function hasAAFCSingleCollectionFilter() {
+    return (filter.hasOwnProperty('collectionCode') &&
+        (filter.collectionCode.length == 1) &&
+        (['CNC', 'DAO'].includes(filter.collectionCode[0])));
+ }
+
+ function updateGetIdButtons() {
+    requestButton = document.getElementById("requestIds");
+    copyButton = document.getElementById("copyIds");
+    filter = getFilter();
+    if (filter) {
+        copyButton.disabled = false;
+        if (hasAAFCSingleCollectionFilter(filter)) {
+            requestButton.disabled = false;
+        } else {
+            requestButton.disabled = true;
+        }
+    } else {
+        copyButton.disabled = true;
+    }
+ }
+
+function get_ids(){
+    if (!filter) return;
+    jqueryString = $.param(filter, true);
+    console.log(jqueryString);
+    apiString = 'https://api.gbif.org/v1/occurrence/search?' + jqueryString + '&limit=100';
+    // still need to convert year range when relevant
+    // other bugs with years
+    console.log(apiString);
+    fetch(apiString)
+    .then(response => response.json())
+    .then(data=> {
+        var ids =  data.results.map(d => d.key);
+        navigator.clipboard.writeText(ids);
+        console.log(ids);
+    });
+ }
+
+ function convertYearRange(yearRangeObject) {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+ }
+
+
+
--- en/data.md
+++ en/data.md
@@ -4,3 +4,29 @@ title: Data
 description: We publish open data
 layout: occurrence
 ---
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<div id="specimenDiv">
+    <button id="requestIds" class="button is-primary" value="" onclick="get_ids(this.value) disabled">Request specimens</button>
+    <button id="copyIds" class="button is-primary disabled" value="" onclick="get_ids(this.value) disabled">Copy occurrence IDs to clipboard</button>
+</div>
+
+<script>
+window.onload = (event) => {
+    let lastUrl = location.href;
+    new MutationObserver(() => {
+      const url = location.href;
+      if (url !== lastUrl) {
+        lastUrl = url;
+        onUrlChange();
+      }
+    }).observe(document, {subtree: true, childList: true});
+};
+
+let filter = null;
+console.log('filter set to null');
+
+function onUrlChange() {
+    updateGetIdButtons();
+}
+</script>
